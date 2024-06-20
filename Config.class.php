0cde459f78e9a0b0011c14027edd85ed9ad227b7
John Brittain <john.skem9@gmail.com>
2024-06-19 23:40:10 -0400

    config

--- Config.class.php
+++ Config.class.php
@@ -4,6 +4,6 @@ namespace JW3B\core\Config;
 class Config {
 	public static $config = [];
 	public function __construct(){
-		self::$config = file_get_contents(__DIR__.'../../config.php');
+		self::$config = file_get_contents(__DIR__.'/../../../config.php');
 	}
 }
