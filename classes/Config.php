b58772dfe00b008ea127d252a6e80a25e8cdc653
John Brittain <john.skem9@gmail.com>
2024-07-11 22:01:50 -0400

    updated default config file

--- classes/Config.php
+++ classes/Config.php
@@ -8,20 +8,20 @@ class Config {
 	public static $t = []; // database tables
 
 	/*
-		@ $path path to config file.
-	*/
-	public function __construct($directory=__DIR__.'/../../../../configs'){
-		self::$c = include($directory.'/config.php');
-		self::$r = include($directory.'/reserved_usernames.php');
-		self::$t = include($directory.'/database_tables.php');
-		self::$l = include($directory.'/language.php');
+	 *@ $path path to config file.
+	 */
+	public function __construct($directory = __DIR__ . '/../../../../lib/config') {
+		self::$c = include ($directory . '/config.php');
+		self::$r = include ($directory . '/reserved_usernames.php');
+		self::$t = include ($directory . '/database_tables.php');
+		self::$l = include ($directory . '/language.php');
 	}
 
-	public static function get($key, $w='c'){
+	public static function get($key, $w = 'c') {
 		return self::$$w[$key] ? self::$$w[$key] : false;
 	}
 
-	public static function set($key, $value, $w){
+	public static function set($key, $value, $w) {
 		self::$$w[$key] = $value;
 		return true;
 	}
